9c16640913667d460b4fffd5cda0273ccfdbb037
Youssef Victor <youssefvictor00@gmail.com>
2021-12-30 15:30:29 +0200

    Initial front end (#4)
    * Initial front end
    
    * Adjustments
    
    * Fix lines
    
    * Send data from backend
    
    * Remove dummy data
    
    * Update project/project/urls.py

--- project/data_model/views.py
+++ project/data_model/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Customer, Car, Reservation
 
-# Create your views here.
+
+def customers(request):
+    customers = Customer.objects.all()
+    return render(request, "customers.html", {"customers": customers})
+
+
+def cars(request):
+    cars = Car.objects.all()
+    return render(request, "cars.html", {"cars": cars})
+
+
+def reservations(request):
+    reservations = Reservation.objects.all()
+    return render(request, "reservations.html", {"reservations": reservations})
