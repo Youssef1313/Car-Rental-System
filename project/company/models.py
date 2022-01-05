54c162b9ee8d962bf471bdc471b0ef478a422c3a
mohamed-zayton <mzayton10@gmail.com>
2022-01-05 12:04:23 +0200

    Create_Tables

--- project/company/models.py
+++ project/company/models.py
@@ -24,7 +24,7 @@ class Reservation(models.Model):
 class Payment(models.Model):
     payment_id = models.AutoField(primary_key=True)
     payment_date = models.DateTimeField()
-    payment_amount = models.CharField(max_length=255)
+    payment_amount = models.PositiveIntegerField()
 
 class Office(models.Model):
     office_id = models.AutoField(primary_key=True)
