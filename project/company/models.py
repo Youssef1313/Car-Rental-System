3871d94f30a36c76dc0204856b17050caaddee52
mohamed-zayton <47240722+mohamed-zayton@users.noreply.github.com>
2022-01-05 12:53:36 +0200

    Model modifications (#17)
    * Create an office model
    * Create a car status model
    * Create a payment model

--- project/company/models.py
+++ project/company/models.py
@@ -1,23 +1,43 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 
-class CarStatus(models.TextChoices):
-    ACTIVE = 'A', 'Active'
-    OUT_OF_SERVICE = 'OOS', 'Out of service'
+class CarStatus(models.Model):
+    id = models.SmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=64)
+
+
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    payment_date = models.DateTimeField()
+    payment_amount = models.PositiveIntegerField()
+
+
+class Office(models.Model):
+    office_id = models.AutoField(primary_key=True)
+    office_name = models.CharField(max_length=255)
+    office_location = models.CharField(max_length=255)
 
 class Car(models.Model):
     plate_id = models.PositiveIntegerField(primary_key=True)
     model = models.CharField(max_length=255)
+    color = models.CharField(max_length=64)
     year = models.PositiveSmallIntegerField()
-
-    status = models.CharField(max_length=8, choices=CarStatus.choices)
+    status = models.ForeignKey(CarStatus, related_name='status', on_delete=models.RESTRICT)
+    belong_office = models.ForeignKey(Office, related_name='office', on_delete=models.CASCADE)
+    is_reserved = models.BooleanField()
 
 class Customer(AbstractUser):
     pass
 
 class Reservation(models.Model):
-    reservation_id = models.PositiveIntegerField(primary_key=True)
-    date = models.DateTimeField()
-    customer = models.ForeignKey(Customer ,related_name='reservation', on_delete=models.CASCADE)
-    car = models.ForeignKey(Car ,related_name='reservation', on_delete=models.CASCADE)
+    reservation_id = models.AutoField(primary_key=True)
+    rental_date = models.DateTimeField()
+    pickup_date = models.DateTimeField(null=True)
+    return_date = models.DateTimeField(null=True)
+    customer = models.ForeignKey(Customer, related_name='reservation', on_delete=models.RESTRICT)
+    car = models.ForeignKey(Car, related_name='reservation', on_delete=models.SET_NULL, null=True)
+    payment = models.ForeignKey(Payment, related_name='reservation', on_delete=models.RESTRICT, null=True)
+
+
+
+
