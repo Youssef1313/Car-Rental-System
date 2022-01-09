c2d9b5f234c7b83465ed4c4c5d45a16f6846cbec
mohamed-zayton <mzayton10@gmail.com>
2022-01-09 09:58:39 +0200

    Search Customer

--- project/company/views/customer_views.py
+++ project/company/views/customer_views.py
@@ -4,23 +4,31 @@ from django.shortcuts import render
 from ..models import Customer
 
 
+def advanced_search(request):
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        return HttpResponseForbidden()
+    pass
+
 def customers(request):
     if not request.user.is_authenticated or not request.user.is_superuser:
         return HttpResponseForbidden()
 
-    if 'customer_search' in request.GET:
-        val = request.GET['customer_search']
+    print(request.GET)
+    if 'search_id' in request.GET:
+        search_id = request.GET['search_id']
+        search_username = request.GET['search_username']
+        search_first_name = request.GET['search_first_name']
+        search_last_name = request.GET['search_last_name']
+        search_email = request.GET['search_email']
+        search_superuser = request.GET['search_superuser']
+        mult_search = Q(Q(id__icontains=search_id)|
+                            Q(username__icontains=search_username)|
+                            Q(first_name__icontains=search_first_name)|
+                            Q(last_name__icontains=search_last_name)|
+                            Q(email__icontains=search_email)|
+                            Q(is_superuser__icontains=search_superuser))
         
-        ##TODO Change this search attr. with the customer attr
-        
-        # mult_search = Q(Q(plate_id__icontains=val)|
-        #                     Q(model__icontains=val)|
-        #                     Q(color__icontains=val)|
-        #                     Q(year__icontains=val)|
-        #                     Q(belong_office__office_name__icontains=val)|
-        #                     Q(belong_office__office_location__icontains=val))
-        
-        # customers = Customer.objects.filter(mult_search)
+        customers = Customer.objects.filter(mult_search)
     else:
         customers = Customer.objects.all()
-    return render(request, "customers.html", {"customers": customers, "title": "Customers"})
+    return render(request, "customers.html", {"customers": customers, "title": "customers"})
