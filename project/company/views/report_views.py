e0b72b6154c83c46d69b46b13c094b1b400ab94c
ahmedabdelkabier <ahmadabdulkabier55@yahoo.com>
2022-01-09 15:24:01 +0200

    add report.html into report folder

--- project/company/views/report_views.py
+++ project/company/views/report_views.py
@@ -13,7 +13,7 @@ def payments_specific_period(request):
     pass
 
 def reports(request):
-    return render(request , "report.html" , {'report' : reports , 'title' : 'Reports'} )
+    return render(request , "report/report.html" , {'report' : reports , 'title' : 'Reports'} )
 
 def periodic_repo(request):
     return render(request , "report/periodic_repo.html" , {'periodic_repo' : periodic_repo , 'title' : 'periodic repo'} )
