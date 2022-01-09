1c5e03b5efe478f350b1b43241afa656455dd462
Youssef Victor <youssefvictor00@gmail.com>
2022-01-09 14:39:41 +0200

    More car fixes (#52)
    * More car fixes
    
    * Fix bug
    
    * Fix bug

--- project/company/views/car_views.py
+++ project/company/views/car_views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.db.models import Q
-from django.http.response import HttpResponseBadRequest, HttpResponseForbidden
+from django.http.response import HttpResponseForbidden
 from django.shortcuts import redirect, render
 from ..models import Car, CarStatus, Office
 
@@ -12,13 +13,15 @@ def cars(request):
         search_year = request.GET['search_year']
         search_office_name = request.GET['search_office_name']
         search_office_location = request.GET['search_office_location']
-    
         mult_search = Q(Q(plate_id__icontains=search_plate_id) &
                         Q(model__icontains=search_model) &
                         Q(color__icontains=search_color) &
                         Q(year__icontains=search_year) &
                         Q(belong_office__office_name__icontains=search_office_name) &
                         Q(belong_office__office_location__icontains=search_office_location))
+
+        if request.GET['search_is_reserved'] != '':
+            mult_search = mult_search & Q(is_reserved=request.GET['search_is_reserved'] == 'true')
 
         if request.GET['car_status'] != '':
             mult_search = mult_search & Q(status__id=request.GET['car_status'])
@@ -36,35 +39,36 @@ def edit_car(request, plate_id):
 
     car = Car.objects.get(pk=plate_id)
     if request.method == "GET":
-        return render(request, "cars/edit_car.html", {'car': car, 'title': 'Edit car'})
+        car_statuses = CarStatus.objects.all()
+        return render(request, "cars/edit_car.html", {'car': car, 'title': 'Edit car', 'car_statuses': car_statuses})
     elif request.method == "POST":
         car.model = request.POST.get('model')
         car.color = request.POST.get('color')
         car.year = request.POST.get('year')
+        car.status = CarStatus(id=request.POST.get('car_status'))
         car.save()
         return redirect(cars)
 
 
 def add_car(request):
     if not request.user.is_authenticated or not request.user.is_superuser:
-        return HttpResponseForbidden()
+        messages.success(request, 'Only admin users can add cars.')
+        return redirect(cars)
 
     if request.method == "GET":
-        return render(request,"cars/add_car.html",{'title' : 'Add Car'})
+        car_statuses = CarStatus.objects.all()
+        offices = Office.objects.all()
+        return render(request,"cars/add_car.html", {'title' : 'Add Car', 'car_statuses': car_statuses, 'offices': offices})
 
     elif request.method == "POST":
         plate_id = request.POST['plate_id']
-        car = Car.objects.filter(pk=plate_id).first()
-        if car is not None:
-            return HttpResponseBadRequest()
-
         model = request.POST['model']
         color = request.POST['color']
         year = request.POST['year']
-        status = CarStatus.objects.get(name = request.POST['status'])
-        belong_office = Office.objects.get(office_name = request.POST['belong_office'])
-
-        data = Car(
+        status = CarStatus.objects.get(pk=request.POST['status'])
+        belong_office = Office.objects.get(pk=request.POST['office'])
+        existing_car = Car.objects.filter(pk=plate_id).first()
+        new_car = Car(
             plate_id = plate_id,
             model = model,
             color = color,
@@ -72,7 +76,12 @@ def add_car(request):
             status = status,
             belong_office = belong_office,
         )
-        data.save()
+
+        if existing_car is not None:
+            messages.success(request, "A car with this plate id already exists")
+            return redirect(add_car, {'car': new_car})
+
+        new_car.save()
     return redirect(cars)
 
 
