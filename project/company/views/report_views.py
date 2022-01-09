ef6eb267e73cbae24b278b682bba61657b1f645b
mohamed-zayton <mzayton10@gmail.com>
2022-01-09 18:03:35 +0200

    fix

--- project/company/views/report_views.py
+++ project/company/views/report_views.py
@@ -1,33 +1,37 @@
 from datetime import datetime, timedelta
+from typing import cast
 from django.contrib import messages
 from django.db import transaction
 from django.db.models import Q,Sum
+from django.db.models.expressions import OuterRef
+from django.db.models.fields import DateField
 from django.http.response import HttpResponseBadRequest, HttpResponseForbidden
 from django.shortcuts import redirect, render
 from ..models import Customer, Reservation, CarStatusConstants, Car, Payment
+from django.db.models.functions import TruncMonth
+from django.db.models.functions import Cast
 
 def specific_customer_reserve(request):
     if not request.user.is_superuser:
         return HttpResponseForbidden()
         
+    reservations = None
     if 'customer_id' in request.GET:
         customer_id = request.GET['customer_id']
         reservations = Reservation.objects.filter(customer__id = customer_id)
-    else:
-        reservations = None
 
     return render(request, "reports/customer_reservation.html", {"reservations":reservations, "title":"Customer reservation"})
 
 def payments_specific_period(request):
     if not request.user.is_superuser:
         return HttpResponseForbidden()
-
+    payments = None
     if 'start_date' in request.GET:
+
         start_date = datetime.fromisoformat(request.GET['start_date'])
         end_date = datetime.fromisoformat(request.GET['end_date'])
-        payments =  Payment.objects.filter(payment_date__range=(start_date, end_date)).values("payment_date").annotate(Sum('payment_amount'))
-    else:
-        payments = None
+        payments =  Payment.objects.filter( payment_date__gte = start_date, payment_date__lte=end_date).annotate(payment_date_only = Cast('payment_date', output_field=DateField())).values('payment_date_only','payment_amount').annotate(Sum('payment_amount'))
+
 
     return render(request, "reports/payment.html", {"payments": payments, "title": "Payment"})
 
