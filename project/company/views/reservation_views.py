d5574d123ad8dba53f81b2707e8f72d01dd6dddf
mohamed-zayton <mzayton10@gmail.com>
2022-01-09 12:57:35 +0200

    Search_Reserved

--- project/company/views/reservation_views.py
+++ project/company/views/reservation_views.py
@@ -7,32 +7,24 @@ from django.shortcuts import redirect, render
 from ..models import Reservation, CarStatusConstants, Car, Payment
 
 
-# TODO: This view is not yet used..
-def reservation_admin(request):
-    if 'reservation_search' in request.GET:
-        val = request.GET['reservation_search']
+def reservations(request):
+    if not request.user.is_authenticated:
+        return HttpResponseForbidden()
 
-        ## TODO Add Customer Id or Customer Name
-        mult_search = Q(Q(id__icontains=val)|
-                         Q(rental_date__icontains=val)|
-                         Q(pickup_date__icontains=val)|
-                         Q(return_date__icontains=val)|
-                         Q(car__plate_id__icontains=val)|
-                         Q(payment__id__icontains=val))
+    if 'search_plate_id' in request.GET:
+        search_plate_id = request.GET['search_plate_id']
+        search_customer_id = request.GET['search_customer_id']
+        search_rental_date = request.GET['search_rental_date']
+        print(request.GET)
+        print(search_rental_date)  
+        print()      
+        mult_search = Q(Q(customer__id__icontains=search_customer_id)&
+                         Q(car__plate_id__icontains=search_plate_id)&
+                         Q(rental_date__lte=search_rental_date))
         
         reservations = Reservation.objects.filter(mult_search)
     else:
         reservations = Reservation.objects.all()
-    return render(request, "reservations/reservation_admin.html", {"reservations": reservations, "title": "Reservations"})
-
-
-def reservations(request):
-    if not request.user.is_authenticated:
-        return HttpResponseForbidden()
-    if request.user.is_superuser:
-        reservations = Reservation.objects.all()
-    else:
-        reservations = request.user.reservations.all()
     return render(request, "reservations/reservations.html", {"reservations": reservations, "title": "Reservations"})
 
 
