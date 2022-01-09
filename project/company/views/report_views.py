faea8af942d8edcd46935b60e06e5da3e7aa4714
mohamed-zayton <mzayton10@gmail.com>
2022-01-09 14:57:36 +0200

    Report_Customer_Payment

--- project/company/views/report_views.py
+++ project/company/views/report_views.py
@@ -1,13 +1,25 @@
 from datetime import datetime, timedelta
 from django.contrib import messages
 from django.db import transaction
-from django.db.models import Q
+from django.db.models import Q,Sum
 from django.http.response import HttpResponseBadRequest, HttpResponseForbidden
 from django.shortcuts import redirect, render
-from ..models import Reservation, CarStatusConstants, Car, Payment
+from ..models import Customer, Reservation, CarStatusConstants, Car, Payment
 
 def specific_customer_reserve(request):
-    pass
+    if not request.user.is_superuser:
+        return HttpResponseForbidden()
+        
+    customer_id = request['customer_id']
+    reservation = Reservation.objects.filter(customer_id = customer_id)
+    return render(request, "reports/report/customer_reservation.html", {{reservation:"reservation", "title":"CustomerReservation"}})
 
 def payments_specific_period(request):
-    pass
+    if not request.user.is_superuser:
+        return HttpResponseForbidden()
+
+    start_date = request['start_date']
+    end_date = request['end_date']
+    reservation =  Reservation.objects.filter(pub_date__range=(start_date, end_date))
+    total_payment = reservation.filter(field_name__isnull=True).aggregate(Sum('payment_id__payment_amount'))
+    return render(request, "reports/report/payment.html", {reservation:"reservation", total_payment:"total_payment", "title":"Payment"})
